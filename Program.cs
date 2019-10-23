8bcaf8f0cead7992d78d5bf64042be5a368c4295
hoangvu1104 <37316295+hoangvu1104@users.noreply.github.com>
2019-10-23 19:56:50 -0400

    Call from main

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
-﻿namespace DatastructureAlgorithms
+﻿using System;
+
+namespace DatastructureAlgorithms
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int[] arr = new int[] { 2, 4, 5, 6, 7, 9, 12, 15, 34, 56 };
+            if (BinarySearch.MyBinarySearch(arr, 7) > 0)
+            {
+                Console.WriteLine("You have it");
+            }
+            else
+            {
+                Console.WriteLine("Get the other one");
+            }
+            Console.ReadLine();
         }
     }
 }
