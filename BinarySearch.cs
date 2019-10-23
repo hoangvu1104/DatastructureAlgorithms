c40908a3fd1ec3e48d611c5d869c12123bc5d9ea
hoangvu1104 <37316295+hoangvu1104@users.noreply.github.com>
2019-10-22 21:41:11 -0400

    Add binary search

---
+++ BinarySearch.cs
@@ -0,0 +1,19 @@
+﻿namespace DatastructureAlgorithms
+{
+    public class BinarySearch
+    {
+        public static int MyBinarySearch(int[] arr, int item)
+        {
+            int low = 0;
+            int high = arr.Length - 1;
+            while(low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                if (arr[mid] == item) return mid;               
+                else if (arr[mid] < item) low = mid + 1;
+                else high = mid - 1;                
+            }
+            return -1;
+        }
+    }
+}
